5e3f63f0389e13b8106303db0b00d21e9e343b2b
datismike <email@email.com>
2023-09-25 22:01:05 +0200

    Alien fleet was added

--- alien_invasion.py
+++ alien_invasion.py
@@ -105,6 +105,29 @@ class AlienInvasion:
         """
         # Create alien.
         alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - 2 * alien_width
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Define row count available.
+        ship_height = self.ship.rect.height
+        available_space_y = self.settings.screen_height - 3 * alien_height - ship_height
+        number_rows = available_space_y // (2 * alien_height)
+
+        # Create first alien row.
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """
+        Create an alien and place him in the row.
+        """
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien_height + 2 * alien_height * row_number
         self.aliens.add(alien)
 
     def _update_screen(self):
--- settings.py
+++ settings.py
@@ -8,15 +8,15 @@ class Settings:
         Initialize game settings.
         """
         # Screen parameters.
-        self.screen_width = 600
-        self.screen_height = 400
+        self.screen_width = 800
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
 
         # Ship parameters.
         self.ship_speed = 0.5
 
         # Bullet parameters.
-        self.bullet_speed = 0.5
+        self.bullet_speed = 1
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
